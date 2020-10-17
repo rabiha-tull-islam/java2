ba573647871058ef3a2a80d3886df63ee24a2865
Rabiha-tull-islam <rabihaislam2@gmail.com>
2020-10-17 19:58:18 +0400

    Programs

---
+++ Myclass.java
@@ -0,0 +1,10 @@
+public class Myclass {
+    public static void main(String[] args) {
+      int x = 20;
+      int y = 68;
+      if (x > y) {
+        System.out.println("x is greater than y");
+      }  
+      
+    }
+  }
---
+++ Myclass1.java
@@ -0,0 +1,11 @@
+  public class Myclass1 {
+    public static void main(String[] args) 
+    {
+      int time = 10;
+      if (time < 18) {
+        System.out.println("Good day.");
+      } else {
+        System.out.println("Good evening.");
+      }  
+    }
+  }
